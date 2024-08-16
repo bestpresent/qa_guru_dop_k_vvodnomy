750b918ee1254588ece8ef17e047b8473f45bb41
Nataly Isaeva <mamadany25@gmail.com>
2024-08-16 09:25:40 +0300

    2 tests

---
+++ test_sign_in.py
@@ -0,0 +1,16 @@
+from selene import browser, be
+
+def test_login_with_valid_data():
+
+    browser.open('https://school.qa.guru/cms/system/login')
+    browser.element('.form-field-email[name=email]').type('lady.astashina@yandex.ru')
+    browser.element('.form-field-password[name=password]').type('Df!3103cz')
+    browser.element('.form-buttons.btn-success').click()
+
+
+    def test_login_with_wrong_password():
+        browser.open('https://school.qa.guru/cms/system/login')
+        browser.element('.form-field-email[name=email]').type('lady.astashina@yandex.ru')
+        browser.element('.form-field-password[name=password]').type('123')
+        browser.element('.form-buttons.btn-success').click()
+        browser.element('login-form.btn-success.btn-error').should(be.visible)
