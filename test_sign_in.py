e4e3849526e802107d323645cc83cd4f22becd2b
Nataly Isaeva <mamadany25@gmail.com>
2024-08-17 09:45:44 +0300

    2 tests

--- test_sign_in.py
+++ test_sign_in.py
@@ -1,4 +1,13 @@
-from selene import browser, be
+from selene import browser, be, have
+
+def test_login_with_wrong_password():
+        browser.open('https://school.qa.guru/cms/system/login')
+        browser.element('.form-field-email[name=email]').type('lady.astashina@yandex.ru')
+        browser.element('.form-field-password[name=password]').type('123')
+        browser.element('.form-buttons.btn-success').click()
+        browser.element('login-form.btn-success.btn-error').should(be.visible).should(be.hidden)
+
+
 
 def test_login_with_valid_data():
 
@@ -6,11 +15,3 @@ def test_login_with_valid_data():
     browser.element('.form-field-email[name=email]').type('lady.astashina@yandex.ru')
     browser.element('.form-field-password[name=password]').type('Df!3103cz')
     browser.element('.form-buttons.btn-success').click()
-
-
-    def test_login_with_wrong_password():
-        browser.open('https://school.qa.guru/cms/system/login')
-        browser.element('.form-field-email[name=email]').type('lady.astashina@yandex.ru')
-        browser.element('.form-field-password[name=password]').type('123')
-        browser.element('.form-buttons.btn-success').click()
-        browser.element('login-form.btn-success.btn-error').should(be.visible)
