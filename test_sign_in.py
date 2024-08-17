e5ebe0e9a56b782b13fd45018dab7f14382e800e
Nataly Isaeva <mamadany25@gmail.com>
2024-08-17 09:57:45 +0300

    2 tests and fix

--- test_sign_in.py
+++ test_sign_in.py
@@ -10,8 +10,8 @@ def test_login_with_wrong_password():
 
 
 def test_login_with_valid_data():
-
     browser.open('https://school.qa.guru/cms/system/login')
     browser.element('.form-field-email[name=email]').type('lady.astashina@yandex.ru')
     browser.element('.form-field-password[name=password]').type('Df!3103cz')
     browser.element('.form-buttons.btn-success').click()
+    browser.element('.header-view').should(have.text('Список тренингов'))
